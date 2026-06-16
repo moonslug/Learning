06547951610d157cad93ab86ba987b3160d13475
RW <fan.wei1987@gmail.com>
2026-06-16 13:14:07 -0400

    C code

---
+++ C/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define SIZE 5  // preprocessor directive. Complier doens't know the var SIZE only sees 5
+
+// const int SIZE = 5; // compiler statement. Complier will go around replace SIZE with 5 
+
+int main()
+{
+    int nums[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("Enter number %d: ", i);
+        scanf("%d", &nums[i]);
+    }
+    return 0;
+}
---
+++ C/tests.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("enter numbers: ");
+    int a;
+    int b;
+    scanf("%d%d", &a, &b);
+    printf("the entered numbers are %d%d\n", a, b);
+    return 0;
+
+}
