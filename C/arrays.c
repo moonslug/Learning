d2367d9f709c0c5783549983991b5d23ed2181e5
RW <fan.wei1987@gmail.com>
2026-06-19 00:35:40 -0400

    more C stuff

--- C/arrays.c
+++ C/arrays.c
@@ -1,18 +1,62 @@
 #include <stdio.h>
+#include <limits.h>
 
 #define SIZE 5  // preprocessor directive. Complier doens't know the var SIZE only sees 5
 
 // const int SIZE = 5; // compiler statement. Complier will go around replace SIZE with 5 
 
 
+
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d: %d", i, arr[i]);
+    }
+    printf("\n");
+}
+
+void inputArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("Enter number %d:", i);
+        scanf("%d", &arr[i]);
+    }
+}
+
+void doubleArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = arr[i] * 2;
+    }
+}
+
+int findMax(int arr[], int length)
+{
+    int tmpMax = INT_MIN;
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i] > tmpMax)
+        {
+            tmpMax = arr[i];
+        }
+    }
+    return tmpMax;
+}
+
 int main()
 {
     int nums[SIZE];
 
-    for (int i = 0; i < SIZE; i++)
-    {
-        printf("Enter number %d: ", i);
-        scanf("%d", &nums[i]);
-    }
+    inputArray(nums, SIZE);
+    printArray(nums, SIZE);
+
+    int max = findMax(nums, SIZE);
+    printf("Max num of array is: %d\n", max);
+
+    doubleArray(nums, SIZE);
+    printArray(nums, SIZE);
     return 0;
 }
--- C/tests.c
+++ C/tests.c
@@ -2,11 +2,26 @@
 
 int main()
 {
-    printf("enter numbers: ");
-    int a;
-    int b;
-    scanf("%d%d", &a, &b);
-    printf("the entered numbers are %d%d\n", a, b);
-    return 0;
+    printf("enter name: ");
+    char myName[10];
+    //scanf("%10s", myName);  // do not use scanf. no protection at the end other than hard code
+    //printf("The name is %s\n", myName);
+    
+    // recommended
+    fgets(myName, 10, stdin);
 
+    // print each ascii to investigate 
+    for (int i = 0; i < 20; i++){
+        printf("%d\n", myName[i]);
+    }
+
+    // replace line break or null terminator with null terminator
+    int j = 0;
+    while (myName[j] != '\n' && myName[j] != '\0')
+        j++;
+    myName[j] = '\0';
+
+    printf("The name is %s!\n", myName);
+
+    return 0;
 }
