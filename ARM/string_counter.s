8f693a40723ea9fc14f399cb81beece3c1c794da
RW <fan.wei1987@gmail.com>
2026-06-20 21:28:07 -0400

    ARM string counter

--- ARM/string_counter.s
+++ ARM/string_counter.s
@@ -1,19 +1,19 @@
-// A program to count the # of char in a given string
-
 .data
 
-msg:    .asciz "Hello ARM!"
+msg:	.asciz "Hello ARM!"
 
 .text
-.global _start
+.global _start 
 _start:
-    ldr r0, =msg
-    mov r1, #0
-    mov r2, #0
-
-    ldrb r1, [r0], #1
-    cmp r1, #0
-    addne r2, #1
-    bne loop
-
+	
+	ldr r0, =msg	// load starting addr of the msg
+	mov r1, #0		// starting r1 to hold the char
+	mov r2, #0		// starting the counter
+	
+loop:
+	ldrb r1, [r0], #1	// load a new char then incr by 1
+	cmp r1, #0			// check for null terminator
+	addne r2, #1		// if not null, add the counter by 1
+	bne loop			// not null keep looping
+	
 .end
